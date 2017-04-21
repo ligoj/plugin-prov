7798681df3bb3790cc4a8323e5405c5996fae9de
Fabrice Daugan <fabrice.daugan@gmail.com>
2017-04-21 21:03:16 +0200

    Fix travis coverage

--- travis.sh
+++ travis.sh
@@ -127,7 +127,7 @@ BUILD)
 
     mvn clean package jacoco:report sonar:sonar \
           $MAVEN_ARGS \
-          -Pjacoco -Djacoco.includes=org.ligoj.app.plugin.vm.* \
+          -Pjacoco -Djacoco.includes=org.ligoj.app.plugin.prov.* \
           -Dsonar.host.url=$SONAR_HOST_URL \
           -Dsonar.organization=ligoj-github \
           -Dsonar.login=$SONAR_TOKEN \
