d667d4d5794bea2f544e4da33bf958055fc991b2
Aldrick <aldrickcleret@gmail.com>
2021-05-03 10:23:55 +0200

    correct problem for icon

--- src/main/resources/META-INF/resources/webjars/service/prov/prov.js
+++ src/main/resources/META-INF/resources/webjars/service/prov/prov.js
@@ -2046,7 +2046,7 @@ define(function () {
 					success: updatedCost => current.defaultCallback(type, updatedCost)
 				});
 			});
-			$('.icon').attr('class',`fa-fw ${current.model.node.tool.uiClasses}`);
+			$('.prov-project .icon').attr('class',`fa-fw ${current.model.node.tool.uiClasses}`);
 			$('.quote-name').text(current.model.configuration.name);
 
 			_('popup-prov-update').on('shown.bs.modal', function () {
