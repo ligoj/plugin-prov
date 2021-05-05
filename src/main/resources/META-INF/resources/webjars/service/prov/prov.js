39b55c23f395de3b624b79ac366682676965cfc9
Aldrick <aldrickcleret@gmail.com>
2021-05-05 14:57:05 +0200

    réctif

--- src/main/resources/META-INF/resources/webjars/service/prov/prov.js
+++ src/main/resources/META-INF/resources/webjars/service/prov/prov.js
@@ -1127,6 +1127,7 @@ define(function () {
 			e.preventDefault();
 			current.save($(this).provType());
 		}).on('show.bs.modal', function (event) {
+			debugger;
 			let $source = $(event.relatedTarget);
 			let dynaType = $source.provType();
 			var $tr = $source.closest('tr');
