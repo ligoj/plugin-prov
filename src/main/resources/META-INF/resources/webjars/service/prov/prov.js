b31c2fbef68af11e8fce9c0cb8972371373c6371
Aldrick <aldrickcleret@gmail.com>
2021-08-18 09:49:10 +0200

    Add 'TB' unit for RAM inputs

--- src/main/resources/META-INF/resources/webjars/service/prov/prov.js
+++ src/main/resources/META-INF/resources/webjars/service/prov/prov.js
@@ -586,13 +586,13 @@ define(function () {
 	 * @param {boolean} showName When true, the type name is displayed. Default is false.
 	 * @return {string} The HTML markup representing the quote storage : type and flags.
 	 */
-	function formatStorageHtml(qs, showName,test1,test2) {
+	function formatStorageHtml(qs, showName) {
 		var type = qs.price.type;
 		return (showName === true ? type.name + ' ' : '') + `<span data-prov-type="storage" data-id="${qs.id}">
 		${formatRate(type.latency)}${type.optimized ? ' ' + formatStorageOptimized(type.optimized) : ''}
-		${qs.quoteInstance ? (qs.quoteInstance.maxQuantity+ 'x'):'' }
+		<small>${qs.quantity && qs.quantity!==1 ? (qs.quantity+ 'x'):'' }</small>
 		${formatManager.formatSize(qs.size * 1024 * 1024 * 1024, 3)}
-		${(qs.size < type.minimal) ? '(' + formatManager.formatSize(type.minimal * 1024 * 1024 * 1024, 3) + ')' : ''}
+		${(qs.size < type.minimal) ? ' (' + formatManager.formatSize(type.minimal * 1024 * 1024 * 1024, 3) + ')' : ''}
 		</span>`;
 	}
 
@@ -1130,11 +1130,12 @@ define(function () {
 		}).on('submit', function (e) {
 			e.preventDefault();
 			current.save($(this).provType());
-		}).on('change',('.mode-advanced input[type=checkbox]'), function (e) {
+		}).on('change','.mode-advanced input[type=checkbox]', function (e) {
+			debugger;
 			if(e.currentTarget.checked){
-				$popup.find('div .element-advanced').removeClass('advanced')
+				$popup.addClass('advanced');	
 			}else{
-				$popup.find('div .element-advanced').addClass('advanced')
+				$popup.removeClass('advanced');
 			}
 		}).on('show.bs.modal', function (event) {
 			let $source = $(event.relatedTarget);
@@ -3013,6 +3014,7 @@ define(function () {
 			qx.usage = usage;
 			qx.budget = budget;
 			qx.resourceType = type;
+			qx.quantity=data.quantity;
 
 			// Specific data
 			current[type + 'CommitToModel'](data, qx);
@@ -3889,20 +3891,25 @@ define(function () {
 							url: REST_PATH + 'service/prov/' + current.model.subscription + '/storage-lookup?' + type + '=' + qi.id,
 							dataType: 'json',
 							data: function (term) {
+								const regex=/(([\d]+)\s*[*x]\s*)?(\d+)/
+								const RexExp =term.match(regex)
 								return {
-									nbQuantity: qi.maxQuantity,
-									size: $.isNumeric(term) ? parseInt(term, 10) : 1, // search term
+									size: $.isNumeric(RexExp[3]) ? parseInt(RexExp[3], 10) : 1, // search term
 								};
 							},
-							results: function (data) {
+							results: function (data,query_page,query) {
+								const regex=/(([\d]+)\s*[*x]\s*)?(\d+)/;
+								const RexExp =query.term.match(regex);
+
 								// Completed the requested identifier
 								data.forEach(quote => {
-									quote.id = quote.price.id + '-' + new Date().getMilliseconds();
-									quote.text = quote.price.type.name;
-								})
+										quote.id = quote.price.id + '-' + new Date().getMilliseconds();
+										quote.text = quote.price.type.name;	
+										quote.quantity= parseInt(RexExp[2])								
+								});
 								return {
 									more: false,
-									results: data,
+									results: data
 								};
 							}
 						}
@@ -3914,6 +3921,7 @@ define(function () {
 								name: current.findNewName(current.model.configuration.storages, qi.name),
 								type: suggest.price.type.code,
 								size: suggest.size,
+								quantity: suggest.quantity,
 								instance: type === 'instance' && qi.id,
 								database: type === 'database' && qi.id,
 								function: type === 'function' && qi.id,
