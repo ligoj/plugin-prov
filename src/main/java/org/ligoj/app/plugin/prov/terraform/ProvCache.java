6b1987f25dafafd82c12c545f47f8089c5894303
Fabrice Daugan <fabrice.daugan@gmail.com>
2026-04-09 23:40:13 +0200

    feat: bump plugin-api 4.1.17

--- src/main/java/org/ligoj/app/plugin/prov/terraform/ProvCache.java
+++ src/main/java/org/ligoj/app/plugin/prov/terraform/ProvCache.java
@@ -3,21 +3,17 @@
  */
 package org.ligoj.app.plugin.prov.terraform;
 
-import java.util.function.Function;
-import java.util.stream.Stream;
-
-import javax.cache.expiry.Duration;
-import javax.cache.expiry.ModifiedExpiryPolicy;
-
+import com.hazelcast.cache.HazelcastCacheManager;
+import com.hazelcast.config.EvictionConfig;
+import com.hazelcast.config.EvictionPolicy;
+import org.ligoj.bootstrap.resource.system.cache.CacheConfigurer;
 import org.ligoj.bootstrap.resource.system.cache.CacheManagerAware;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Role;
 import org.springframework.stereotype.Component;
 
-import com.hazelcast.cache.HazelcastCacheManager;
-import com.hazelcast.config.CacheConfig;
-import com.hazelcast.config.EvictionConfig;
-import com.hazelcast.config.EvictionPolicy;
+import javax.cache.expiry.Duration;
+import java.util.stream.Stream;
 
 /**
  * Provisioning data cache configurations.
@@ -27,37 +23,36 @@ import com.hazelcast.config.EvictionPolicy;
 public class ProvCache implements CacheManagerAware {
 
 	@Override
-	public void onCreate(final HazelcastCacheManager cacheManager, final Function<String, CacheConfig<?, ?>> provider) {
-		cacheManager.createCache("terraform-version", provider.apply("terraform-version"));
-		final var cfgTVL = provider.apply("terraform-version-latest")
-				.setExpiryPolicyFactory(ModifiedExpiryPolicy.factoryOf(Duration.ONE_DAY));
+	public void onCreate(final HazelcastCacheManager cacheManager, final CacheConfigurer configurer) {
+		cacheManager.createCache("terraform-version", configurer.newCacheConfig("terraform-version"));
+		final var cfgTVL = configurer.newCacheConfig("terraform-version-latest",Duration.ONE_DAY);
 		cacheManager.createCache("terraform-version-latest", cfgTVL);
 
-		final var cfgPL = provider.apply("prov-location")
+		final var cfgPL = configurer.newCacheConfig("prov-location")
 				.setEvictionConfig(new EvictionConfig().setEvictionPolicy(EvictionPolicy.LRU).setSize(1000));
 		cacheManager.createCache("prov-location", cfgPL);
 
 		// Instance cache configurations
-		createCacheEvict(cacheManager, provider, "prov-instance-type", "prov-instance-type-dyn",
+		createCacheEvict(cacheManager, configurer, "prov-instance-type", "prov-instance-type-dyn",
 				"prov-instance-type-has-dyn", "prov-instance-has-co2", "prov-instance-term", "prov-database-type",
 				"prov-database-type-dyn", "prov-database-type-has-dyn", "prov-database-has-co2", "prov-container-type",
 				"prov-container-type-dyn", "prov-container-type-has-dyn", "prov-container-has-co2",
 				"prov-function-type", "prov-function-type-dyn", "prov-function-type-has-dyn", "prov-function-has-co2");
-		createCache(cacheManager, provider, "prov-processor", "prov-instance-software", "prov-instance-license",
+		createCache(cacheManager, configurer, "prov-processor", "prov-instance-software", "prov-instance-license",
 				"prov-instance-os", "prov-database-engine", "prov-database-edition", "prov-database-license",
 				"prov-container-license", "prov-container-os", "prov-architecture");
 
 	}
 
 	private void createCache(final HazelcastCacheManager cacheManager,
-			final Function<String, CacheConfig<?, ?>> provider, final String... names) {
-		Stream.of(names).forEach(name -> cacheManager.createCache(name, provider.apply(name)));
+			final CacheConfigurer configurer, final String... names) {
+		Stream.of(names).forEach(name -> cacheManager.createCache(name, configurer.newCacheConfig(name)));
 	}
 
 	private void createCacheEvict(final HazelcastCacheManager cacheManager,
-			final Function<String, CacheConfig<?, ?>> provider, final String... names) {
+			final CacheConfigurer configurer, final String... names) {
 		Stream.of(names).forEach(name -> {
-			final var cfgPIT = provider.apply(name)
+			final var cfgPIT = configurer.newCacheConfig(name)
 					.setEvictionConfig(new EvictionConfig().setEvictionPolicy(EvictionPolicy.LRU).setSize(1000));
 			cacheManager.createCache(name, cfgPIT);
 		});
